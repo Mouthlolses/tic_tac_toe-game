a37c544d8ef525c1f42e0fe8576972fd689ca2e5
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2025-06-18 12:45:34 -0300

    second_commit

--- game.cpp
+++ game.cpp
@@ -1,29 +1,29 @@
 #include <iostream>
 using namespace std;
 
-char tabuleiro[3][3] = {
+char board[3][3] = {
     {'1','2','3'},
     {'4','5','6'},
     {'7','8','9'}
 };
 
-void exibirTabuleiro() {
-    cout << tabuleiro[0][0] << " | " << tabuleiro [0][1] << " | " << tabuleiro[0][2] << endl;
+void displayBoard() {
+    cout << board[0][0] << " | " << board [0][1] << " | " << board[0][2] << endl;
     cout << "---------\n";
-    cout << tabuleiro[1][0] << " | " << tabuleiro[1][1] << " | " << tabuleiro[1][2] << endl;
+    cout << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << endl;
     cout << "---------\n";
-    cout << tabuleiro[2][0] << " | " << tabuleiro[2][1] << " | " << tabuleiro[2][2] << endl;
+    cout << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << endl;
 
 }
 
 bool winner()
 {
     for(int i = 0; i < 3; i++) {
-        if (tabuleiro[i][0] == tabuleiro[i][1] && tabuleiro[i][1] == tabuleiro[i][2]) return true;
-        if (tabuleiro[0][i] == tabuleiro[1][i] && tabuleiro[1][i] == tabuleiro[2][i]) return true;
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2]) return true;
+        if (board[0][i] == board[1][i] && board[1][i] == board[2][i]) return true;
     }
-    if (tabuleiro[0][0] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][2]) return true;
-    if (tabuleiro[0][2] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][0]) return true;
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) return true;
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) return true;
 
     return false;
 }
@@ -32,46 +32,46 @@ bool loot()
 {
     for(int i = 0; i < 3; i++) {
         for(int j = 0; j < 3; j++) {
-            if (tabuleiro[i][j] != 'X' && tabuleiro[i][j] != 'O') return false;
+            if (board[i][j] != 'X' && board[i][j] != 'O') return false;
         }
     }
     return true;
 }
 
-void fazerJogada(char jogador)
+void makeMove(char player)
 {
     int pos;
-    cout << "Jogador " << jogador << ", escolha uma posicao (1-9): "; cin >> pos;
+    cout << "Player " << player << ", escolha uma posicao (1-9): "; cin >> pos;
 
     int i = (pos - 1) / 3;
     int j = (pos - 1) % 3;
 
-    if (tabuleiro[i][j] == 'X' || tabuleiro[i][j] == 'O') {
+    if (board[i][j] == 'X' || board[i][j] == 'O') {
         cout << "Posicao ocupada! Tente novamente.\n";
-        fazerJogada(jogador);
+        makeMove(player);
     } else {
-        tabuleiro[i][j] = jogador;
+        board[i][j] = player;
     }
 }
 
 int main()
 {
-    char jogador = 'X';
+    char player = 'X';
 
     while (true) {
-        exibirTabuleiro();
-        fazerJogada(jogador);
+        displayBoard();
+        makeMove(player);
 
         if (winner()) {
-            exibirTabuleiro();
-            cout << "Jogador " << jogador << " venceu!\n";
+            displayBoard();
+            cout << "Player " << player << " winner!\n";
             break;
         }
         if (loot()) {
-            exibirTabuleiro();
+            displayBoard();
             cout << "Deu velha!\n";
             break;
         }
-        jogador = (jogador == 'X') ? 'O' : 'X';
+        player = (player == 'X') ? 'O' : 'X';
     }
 }
